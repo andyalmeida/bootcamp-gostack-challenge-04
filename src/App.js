8a79a96a2ff8d366900a87c4edae98c9843b73d5
André Almeida <andalvalmeida@gmail.com>
2019-11-02 23:28:36 -0300

    Dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 import Header from './components/Header';
 import PostList from './components/PostList';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <PostList />      
-    </>
-  );
+class App extends Component {
+  
+  state = {
+    theme: ''
+  }
+
+  componentDidMount() {
+    let theme = localStorage.getItem('theme');
+    if(!theme) theme = 'light';
+    this.setState({ theme });
+    document.documentElement.setAttribute('data-theme', theme);
+
+  }
+
+  handleChangeTheme() {
+    const theme = this.state.theme === 'light' ? 'dark' : 'light';
+    this.setState({ theme });    
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+
+  render() {
+    return (
+      <>
+        <Header onChangeTheme={() => this.handleChangeTheme()} />
+        <PostList />      
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import logo from '../assets/logo.png';
 import account_icon from '../assets/account_icon.png';
+import theme_icon from '../assets/theme_icon.png';
 
-function Header() {
+function Header({ onChangeTheme }) {
   return (
     <div id="header">
       <img src={logo} alt="Facebook"/>
       <div>
         <a href="#">Meu perfil</a>
         <img src={account_icon} alt="account"/>
+        <img src={theme_icon} onClick={onChangeTheme} alt="theme"/>
       </div>
     </div>
   );
+}
+
+Header.propTypes = {
+  onChangeTheme: PropTypes.func.isRequired
 }
 
 export default Header;
